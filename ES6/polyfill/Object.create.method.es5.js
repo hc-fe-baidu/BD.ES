f52eafa5fd40d467593ca337012a39b3e89142da
hcforbaidu <hucheng@baidu.com>
2016-09-26 21:27:07 +0800

    some file

--- ES6/polyfill/Object.create.method.es5.js
+++ ES6/polyfill/Object.create.method.es5.js
@@ -1,3 +1,7 @@
+ //  https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+
+// 为了让其进入 if, 我先注释了正确的判断
+// if (typeof Object.create != 'function') {
 if (typeof Object.create == 'function') {
     // Production steps of ECMA-262, Edition 5, 15.2.3.5
     // Reference: http://es5.github.io/#x15.2.3.5
@@ -20,7 +24,10 @@ debugger;
             //    Object是标准内置的构造器名
             // 3. 设置 obj 的内部属性 [[Prototype]] 为 O。
             Temp.prototype = O;
+
             var obj = new Temp();
+
+            // 精彩
             Temp.prototype = null; // 不要保持一个 O 的杂散引用（a stray reference）...
 
             // 4. 如果存在参数 Properties ，而不是 undefined ，
@@ -31,6 +38,7 @@ debugger;
                 // Object.defineProperties does ToObject on its first argument.
                 var Properties = Object(arguments[1]);
                 for (var prop in Properties) {
+                    // 为什么还要判断一次? for in 会遍历得到原型中可枚举的属性.但这不是他自身的属性. 原型中的属性已经可以访问.如果再拷贝到 obj 上就违背了继承的初衷.
                     if (hasOwn.call(Properties, prop)) {
                         obj[prop] = Properties[prop];
                     }
@@ -43,4 +51,24 @@ debugger;
     })();
 }
 
- // TODO: 研究create进行继承   https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+let liming = Object.create({
+    show() {
+        console.log(this);
+    }
+}, {
+    __proto__: {
+        a: 1,
+        b: 2
+    },
+    id: {
+        value: 1001,
+        enumerable: true,
+        configurable: false
+    },
+
+    name: {
+        value: true,
+        enumerable: true,
+        writable: true
+    }
+});
